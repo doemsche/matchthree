dedfcc71ffc2a318bb2e512aa2d10226d88801e2
Dominik Schlaepfer <doemsche@gmx.ch>
2016-12-21 17:23:56 +0100

    initial commit

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,113 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Board : MonoBehaviour {
+
+	public int width;
+	public int height;
+
+	public int borderSize;
+
+	public GameObject tilePrefab;
+	public GameObject[] gamePiecePrefabs;
+
+	Tile[,] m_allTiles;
+	GamePiece[,] m_allGamePieces;
+
+	Tile m_clickedTile;
+	Tile m_targetTile;
+
+	// Use this for initialization
+	void Start () {
+		m_allTiles = new Tile[width,height];
+		m_allGamePieces = new GamePiece[width,height];
+		SetupTiles();
+		SetupCamera();
+		FillRandom();
+	}
+
+	void SetupTiles(){
+		for(int i = 0; i < width; i++){
+			for(int j = 0; j < height; j++){
+				GameObject tile = Instantiate(tilePrefab, new Vector3(i,j,0), Quaternion.identity) as GameObject;
+
+				tile.name = "Tile ("+i+","+j+")";
+
+				m_allTiles[i,j] = tile.GetComponent<Tile>();
+
+				tile.transform.parent = transform;
+
+				m_allTiles[i,j].Init(i,j,this);
+			}
+		}
+	}
+
+	void SetupCamera(){
+		Camera.main.transform.position = new Vector3( (float)(width-1)/2f, (float)(height-1)/2f, -10f);
+
+		float aspectRation = (float) Screen.width / (float) Screen.height;
+
+		float verticalSize = height / 2f + (float) borderSize;
+
+		float horizontalSize = ((float)width / 2f +(float)borderSize) / aspectRation;
+
+		Camera.main.orthographicSize = (verticalSize > horizontalSize) ? verticalSize : horizontalSize;
+
+	}
+
+	GameObject GetRandomGamePiece(){
+		int randomIdx = Random.Range(0, gamePiecePrefabs.Length);
+
+		if(gamePiecePrefabs[randomIdx] == null){
+			Debug.LogWarning("BOARD: does not contain a vali dGamepiece prefab");
+		}
+	
+		return gamePiecePrefabs[randomIdx];
+	}
+
+	void PlaceGamePiece(GamePiece gamePiece, int x, int y){
+		if(gamePiece == null){
+			Debug.LogWarning("BOARD: Invalid Game Piece.");
+			return;
+		}
+
+		gamePiece.transform.position = new Vector3(x,y,0);
+		gamePiece.transform.rotation = Quaternion.identity;
+		gamePiece.SetCoord(x,y);
+	}
+
+	void FillRandom(){
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < height; j++) {
+				GameObject randomPiece = Instantiate(GetRandomGamePiece(),Vector3.zero,Quaternion.identity) as GameObject;
+
+				if(randomPiece != null){
+					PlaceGamePiece(randomPiece.GetComponent<GamePiece>(), i, j);
+				}
+			}
+		}
+	}
+
+	public void ClickTile(Tile tile){
+		if(m_clickedTile == null){
+			m_clickedTile = tile;
+		}
+	}
+
+	public void DragToTile(Tile tile){
+		if(m_clickedTile != null){
+			m_targetTile = tile;
+		}	
+	}
+
+	public void ReleaseTile(){
+		if(m_clickedTile != null && m_targetTile != null){
+			SwitchTiles(m_clickedTile,m_targetTile);
+		}
+	}
+
+	void SwitchTiles(Tile clickedTile, Tile targetTile){
+		m_clickedTile = null;
+		m_targetTile = null;
+	}
+}
---
+++ Assets/Scripts/GamePiece.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GamePiece : MonoBehaviour {
+
+	public int xIndex;
+	public int yIndex;
+
+	bool m_isMoving = false;
+
+	public enum InterpType{
+		Linear,
+		EaseOut,
+		EaseIn,
+		SmoothStep,
+		SmootherStep
+	};
+
+	public InterpType interpolation = InterpType.SmootherStep;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+//		if(Input.GetKey(KeyCode.RightArrow)){
+//			Move((int)transform.position.x +2, (int)transform.position.y, 0.5f);
+//		}
+//
+//		if(Input.GetKey(KeyCode.LeftArrow)){
+//			Move((int)transform.position.x -2, (int)transform.position.y, 0.5f);
+//		}	
+	}
+
+	public void SetCoord(int x, int y){
+		xIndex = x;
+		yIndex = y;
+	}
+
+	public void Move(int destX, int destY, float timeToMove){
+		if(!m_isMoving){
+			StartCoroutine( MoveRoutine( new Vector3(destX,destY,0), timeToMove) ) ;	
+		}
+	}
+
+	IEnumerator MoveRoutine(Vector3 destination, float timeToMove){
+		Vector3 startPosition = transform.position;
+
+		bool reachedDestination = false;
+
+		float elapsedTime = 0f;
+
+		m_isMoving = true;
+
+		while (!reachedDestination){
+
+			//if we are close enought ot destination
+			if(Vector3.Distance(transform.position, destination) < 0.01f){
+				reachedDestination = true;
+				transform.position = destination;
+				SetCoord( (int)destination.x, (int)destination.y );
+				break;
+			}
+
+			elapsedTime += Time.deltaTime;
+
+			float t = Mathf.Clamp( elapsedTime / timeToMove, 0f, 1f);
+
+			switch(interpolation){
+				case InterpType.Linear:
+					break;
+				case InterpType.EaseOut:
+					t = Mathf.Sin(t* Mathf.PI * 0.5f);
+					break;	
+				case InterpType.EaseIn:
+					t = 1 - Mathf.Cos(t*Mathf.PI * 0.5f);
+					break;	
+				case InterpType.SmoothStep:
+					t = t*t*(3-2*t);
+					break;	
+				case InterpType.SmootherStep:
+					t = t*t*t*(t*(t*6-15)+10);
+					break;	
+			}
+				
+			transform.position = Vector3.Lerp(startPosition, destination, t);
+
+			//wait until the next frame
+			yield return null;
+		}
+
+		m_isMoving = false;
+	}
+}
